857ac1b867c8ba026ff39d5925c7cd39b3711c3e
Charles Dunn <dunnc1551@gmail.com>
2017-03-14 12:32:44 -0600

    Add iteration1 - part 1. Tests are passing and data structure is complete.

--- src/helper.js
+++ src/helper.js
@@ -4,50 +4,50 @@ export default class DistrictRepository {
   }
 
   removeDuplicates(data) {
-    const testData = data.reduce((acc, obj) => {
-      if (!acc[obj.Location]) {
-        acc[obj.Location.toUpperCase()] = [];
+    let mapped = data.map(obj => {
+       obj.Location = obj.Location.toUpperCase();
+       return obj;
+    });
+    return mapped.reduce((acc, val)=> {
+      if (!acc[val.Location]) {
+        acc[val.Location] = { locationData: [],
+                              location: val.Location,
+                              data: this.sanitizeData(data)
+                            };
       }
-      acc[obj.Location.toUpperCase()].push(obj)
+      acc[val.Location].locationData.push(val);
       return acc;
     }, {});
-    return testData;
   }
 
   findByName(name) {
-    let upperName = name.toUpperCase();
-
-    let keys = Object.keys(this.data);
-
-    if(!upperName || !keys.includes(upperName)){
-      return undefined
+    if (!name) {
+      return undefined;
     }
+    const upperCaseName = name.toUpperCase();
+    const keys = Object.keys(this.data);
+    if (!keys.includes(upperCaseName)) {
+      return undefined;
+    }
+    const dataArray = this.data[upperCaseName].locationData;
+    this.sanitizeData(dataArray);
 
-    const location = this.data[upperName][0]
-    console.log(location)
-    return location
+    return this.data[upperCaseName];
+  }
 
+  sanitizeData(data) {
+    const sanitized = data.map(location => {
+      typeof location.Data !== 'number' ? location.Data = 0 : location.Data;
+      return location;
+    });
+    return this.formatData(sanitized);
+  }
+
+  formatData(data) {
+    return data.reduce((obj, val) => {
+      obj[val.TimeFrame] = val.Data
+      return obj;
+    }, {});
   }
 
 }
-
-// let upperName = name.toUpperCase()
-//
-// // const regex = new RegExp(name, 'i')
-//
-// let mapped = keys.map((key) => {
-//   return key.toUpperCase()
-//   // let match = key.match(regex)
-//   // return match
-// })
-//
-// let filtered = mapped.filter(key => {
-//   return key === upperName
-// })
-
-
-
-
-
-
-
