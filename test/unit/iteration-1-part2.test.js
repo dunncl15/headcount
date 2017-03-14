e754f1da4e62efd9de76d4ac7bc0785b9fdf9303
Charles Dunn <dunnc1551@gmail.com>
2017-03-14 15:13:09 -0600

    Add findAllMatches function and test all iterations

--- test/unit/iteration-1-part2.test.js
+++ test/unit/iteration-1-part2.test.js
@@ -1,15 +1,15 @@
 import DistrictRepository from '../../src/helper.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
-describe.skip('DistrictRepository iteration 1 - part 2', () =>  {
+describe('DistrictRepository iteration 1 - part 2', () =>  {
   const district = new DistrictRepository(kinderData);
 
-  test('findAllMatches defaults to returning all data in an array', () => {
-    expect(district.findAllMatches().length).toBe(181);
+  test('findAllMatches defaults to returning all data in an object', () => {
+    expect(Object.keys(district.findAllMatches()).length).toBe(181);
   });
 
   test('findAllMatches returns matches in an array, case insensitive', () => {
-    expect(district.findAllMatches('ColoRado').length).toBe(2);
+    expect(district.findAllMatches('ColoRado').length).toBe(11);
   });
 
   test('findAllMatches finds no matches and returns an empty array when given arguments that dont exist within the data', () => {
